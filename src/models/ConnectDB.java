ab160c49464cfa49b9975d689a923d74833e0702
mandox <fly82@ukr.net>
2014-02-03 11:14:46 +0200

    getConnectionMSSQL

--- src/models/ConnectDB.java
+++ src/models/ConnectDB.java
@@ -1,4 +1,35 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getConnection;
+
 public class ConnectDB {
+
+    public Connection getConnectionH2() throws ClassNotFoundException {
+        try {
+            Class.forName("org.h2.Driver");
+
+            return getConnection("jdbc:h2:../.appeals.h2.db", "sa", "sa");
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnectionMSSQL() throws ClassNotFoundException {
+        final String SERVER = "sqlserver://192.168.200.3:1433";
+        final String PASS = "PrDFk267";
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            return getConnection("jdbc:jtds:" + SERVER + ";DatabaseName=Zvertan", "sa", PASS);
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
